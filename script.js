472564890859e2774110cc568b2fd11edbb28307
vpgits <vp.mb@protonmail.com>
2023-06-18 22:55:44 +0530

    mobile optimized @ 960px

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    hamburgMenu();
+  });
+
 window.onscroll = function() {headerShrink()};
 
 function headerShrink(){
     if (document.body.scrollTop > 50){document.getElementById("header").style.fontSize = "20px;"}
 }
+
+function hamburgMenu(){
+    const element = document.getElementById("links");
+    if (element.className === "header-links") element.className+='-hamburg';
+    else element.className = "header-links";
+}
+
+const menuButton = document.getElementById("hamburgMenu");
+menuButton.addEventListener('click', hamburgMenu);
