61c5c8f278966336339b8a92ba4649174f4407c4
vpgits <vp.mb@protonmail.com>
2023-07-03 10:40:16 +0530

    payments page

--- student_1/payment_page.js
+++ student_1/payment_page.js
@@ -11,22 +11,24 @@ function validateName(inputelement, parentelement){
         const warningText = document.querySelector(".warning."+inputelement);
         const parentElement = document.getElementById(parentelement);
         warning.classList.add(inputelement);
-        if (warningText === null) 
+        var regex =  /^[A-Za-z\s]+$/;
+        if (warningText === null) {
             parentElement.appendChild(warning);
-        var regex =  /^[A-Za-z]+$/;
-        warning.textContent = null;
-        if (text.length <7){
-            warningText.textContent  =  "Should enter first and second name";
+        } else{
+            warning.textContent = null;
+            if (text.length <7){
+                warningText.textContent  =  "Should enter first and second name";
+            }
+            else if (text.length >= 7){
+                if (!regex.test(text)){
+                    warningText.textContent  =  "Should exclude symbols and numbers";
+                   
+                }else {
+                    validName = true;
+                    parentElement.removeChild(warning);
+            } 
         }
-        else if (text.length >= 7){
-            if (!regex.test(text)){
-                warningText.textContent  =  "Should exclude symbols and numbers";
-               
-            }else {
-                validName = true;
-                parentElement.removeChild(warningText);
-        } 
-    }
+        }
     })
     console.log(validName);
     return validName;
@@ -38,23 +40,23 @@ function validateNumber(){
     validTele = false;
     let element = document.getElementById("telephone");
     const warningText = document.querySelector(".warning.telephone");
-        const parentElement = element.parentNode;
-        warning.classList.add("telephone");
-        if (warningText === null) 
-        console.log(element.value)
+    const parentElement = element.parentNode;
+    warning.classList.add("telephone");
+    if (warningText === null) {
         parentElement.appendChild(warning);
+    } else {
         warning.textContent = null;
-    if ( element.value.length !=0){
-        if (element.value.length != 10){
-            warningText.textContent = "Enter a valid phone number";
-           
-        } else if (element.value.length === 10) {
-            parentElement.removeChild(warningText);
-            validTele = true;
-      
+        if ( element.value.length !=0){
+            if (element.value.length != 10){
+                warningText.textContent = "Enter a valid phone number";
+               
+            } else if (element.value.length === 10) {
+                parentElement.removeChild(warning);
+                validTele = true;
+          
+            }
         }
     }
-    console.log(validTele);
     return validTele;
 }
 
@@ -64,14 +66,16 @@ function validateEmail(){
     let email = document.getElementById("email");
     var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //chatgpt
     const warningText = document.querySelector(".warning.email");
-        const parentElement  = email.parentNode;
-        warning.classList.add("email");
-        if (warningText === null) 
-            parentElement.appendChild(warning);
-    if(!regex.test(email.value)){
-        warningText.textContent  = "Enter a valid email";
-    } else{
-        parentElement.removeChild(warningText);
+    const parentElement  = email.parentNode;
+    warning.classList.add("email");
+    if (warningText === null){
+        parentElement.appendChild(warning);
+    } else {
+        if(!regex.test(email.value)){
+            warningText.textContent  = "Enter a valid email";
+        } else{
+            parentElement.removeChild(warning);
+        }
     }
 }
 
@@ -80,22 +84,23 @@ function validateCreditCardNum(){
     const warningText  =document.querySelector(".warning.number");
     const parentElement = num.parentNode;
     warning.classList.add("number");
-    var regex = /^\d+$/;
+    var regex = /^\d{4}-\d{4}-\d{4}-\d{4}$/;
     if (warningText === null){
         parentElement.appendChild(warning);
-    }
-    if (regex.test(num.value)){
-        if (num.value.length!=16){
-            warningText.textContent = "A valid card number must have 16 digits";
-            
-        }else {
-            console.log("here");
-            parentElement.removeChild(warningText);
+    } else{
+        if (num.value.length<19 && (num.value.length===4 || num.value.length==9|| num.value.length ===14)){
+            num.value += "-";
         }
-    } else {
-        warningText.textContent = "Enter digits";
-        // parentElement.appendChild(warningText);
-
+        if (regex.test(num.value)){
+            if (num.value.length!=19){
+                warningText.textContent = "A valid card number must have 16 digits";
+            }else {
+                parentElement.removeChild(warning);
+            }
+        } else {
+            warningText.textContent = "Enter digits";
+            // parentElement.appendChild(warningText);
+        }
     }
 }
 
@@ -107,20 +112,39 @@ function validateExpiryDate(){
     var regex = /^\d+\/\d+$/; //chatgpt formatted like so checks for 12/24 format
     if (warningText === null){
         parentElement.appendChild(warning);
-    }
-    if (!regex.test(date.value)){
-        warningText.textContent = "Enter in the given format MM/YY";
     } else{
-        if (date.value.slice(0,2)>12){
-            warningText.textContent = "Month is invalid";
-        } else if (date.value.length != 5){
-            warningText.textContent = "Invalid Year";
-        } else {
-            parentElement.removeChild(warning);
+        if (!regex.test(date.value)){
+            warningText.textContent = "Enter in the given format MM/YY";
+        } else{
+            if (date.value.slice(0,2)>12){
+                warningText.textContent = "Month is invalid";
+            } else if (date.value.length != 5){
+                warningText.textContent = "Invalid Year";
+            } else {
+                parentElement.removeChild(warning);
+            }
         }
     }
 }
 
+function validateHouseNumber(){
+    let regex =/^(\d+)(\/\d*)?$/;
+    const number = document.getElementById("houseNo");
+    const warningText = document.querySelector(".warning.houseNo");
+    const parentElement = number.parentElement;
+    warning.classList.add("houseNo");
+    if (warningText === null){
+        parentElement.appendChild(warning);
+    } else {
+        if (!regex.test(number.value)){
+            console.log('Im here');
+            warning.textContent = "Enter a valid House number"
+        } else {
+            parentElement.removeChild(warning);
+        }
+    }
+
+}
 
 
 
@@ -141,4 +165,8 @@ function switchFormsOne(){
     document.getElementById("payment-details").classList.add("active");
     document.querySelector("footer").style.position = "relative";
     document.getElementById("user-details").style.marginTop = 0;
+    document.querySelectorAll(".btns.formOne").forEach((element) =>{
+        element.style.display = "none";
+    })
+      
 }
