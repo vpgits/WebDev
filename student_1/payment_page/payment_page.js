f62b7f095bad0007720e731f76f8e5545204afea
Heshan-w <107771123+Heshan-w@users.noreply.github.com>
2023-07-24 12:42:26 +0530

    major changes done to the HTML and JS files

--- student_1/payment_page/payment_page.js
+++ student_1/payment_page/payment_page.js
@@ -1,28 +1,33 @@
 // Student Role: student 1
 // Student Name: Heshan Wanigasinghe
 
-//creates a warning element to be appended to the DOM
+// Create a warning element for displaying validation messages
 const warning = document.createElement("p");
 warning.classList.add("warning");
 
+// Event listener to run code after the DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
+  // Retrieve cart data from local storage and display it on the page
   let cart = window.localStorage.getItem("cart");
-
+  // Remove the "Proceed To Checkout" button from the cart data
   cart = cart.replace(
     '<button class="btn btn-primary btn-ProceedToCheckout" type="button">Proceed To Checkout</button>',
     ""
   );
-
+  // Display the cart data on the page
   let cartCard = document.querySelector(".cartCard");
   cartCard.innerHTML = cart;
+
+  // Make all input elements in the cart read-only
   var inputElements = cartCard.querySelectorAll("input");
   for (var i = 0; i < inputElements.length; i++) {
     inputElements[i].readOnly = true;
   }
 });
 
-// validates the name
+// Function to validate the name input
 function validateName(inputelement, parentelement) {
+  // Get the input element and its value
   let element = document.getElementById(inputelement);
   const text = element.value;
   const warningText = document.querySelector(".warning." + inputelement);
@@ -32,7 +37,11 @@ function validateName(inputelement, parentelement) {
   if (warningText !== null) {
     parentElement.removeChild(warningText);
   }
+
+  // Check if the name is valid using isNameValid function
   const isValid = isNameValid(text);
+
+  // If the name is invalid, display a warning message
   if (!isValid) {
     const warning = document.createElement("p");
     warning.classList.add("warning", inputelement);
@@ -40,10 +49,14 @@ function validateName(inputelement, parentelement) {
     warning.style.fontSize = "14px";
     parentElement.appendChild(warning);
   }
+
+  // Return the validity status of the name
   return isValid;
 }
 
+// Function to check if the name is valid (n input without symbols and/or numbers)
 function isNameValid(name) {
+  // loops through each letter in the entered name to check if an invalid character is entered
   for (let i = 0; i < name.length; i++) {
     const charCode = name.charCodeAt(i);
     // Check if the character is a letter (A-Z or a-z) or a space
@@ -52,26 +65,31 @@ function isNameValid(name) {
       !(charCode >= 97 && charCode <= 122) && // a-z
       charCode !== 32 // Space
     ) {
+      // returns false if name is invalid and true if valid
       return false;
     }
   }
   return true;
 }
 
-// validates the  numbers
+// Function to validate numeric input
 function validateNumber(inputelement, length, warningmsg) {
   let element = document.getElementById(inputelement);
   const warningText = document.querySelector(`.warning.${inputelement}`);
   const parentElement = element.parentNode;
   warning.classList.add(`${inputelement}`);
+
+  // Check if the warning element already exists in the parent element
   if (warningText === null) {
     parentElement.appendChild(warning);
   } else {
     warning.textContent = null;
+    // checks if the number of digits in the number is not equal to zero or the expected length
     if (element.value.length != 0) {
       if (element.value.length != length) {
         warningText.textContent = warningmsg;
         return false;
+        // if the number of digis=ts in the name is equal to the expected length, return true
       } else if (element.value.length == length) {
         parentElement.removeChild(warning);
         return true;
@@ -80,7 +98,7 @@ function validateNumber(inputelement, length, warningmsg) {
   }
 }
 
-// validates the email
+// Function to validate email input
 function validateEmail() {
   let email = document.getElementById("email");
   const warningText = document.querySelector(".warning.email");
@@ -90,29 +108,29 @@ function validateEmail() {
   if (warningText === null) {
     parentElement.appendChild(warning);
   } else {
+    // If the email is not valid, display a warning message
     if (!isEmailValid(email.value)) {
       warningText.textContent = "Enter a valid email";
       return false;
     } else {
+      // If the email is valid, remove the warning message
       parentElement.removeChild(warning);
       return true;
     }
   }
 }
 
+// Function to check if the email is valid
 function isEmailValid(email) {
   // Check if email contains both "@" and "."
   const atIndex = email.indexOf("@");
   const dotIndex = email.lastIndexOf(".");
-
-  if (atIndex < 1 || dotIndex <= atIndex + 1 || dotIndex === email.length - 1) {
-    return false;
-  } else {
-    return true;
-  }
+  return (
+    atIndex >= 1 && dotIndex > atIndex + 1 && dotIndex !== email.length - 1
+  );
 }
 
-// validates the expiry date
+// Function to validate the expiry date input
 function validateExpiryDate() {
   const date = document.getElementById("expiry-date");
   const warningText = document.querySelector(".warning.expiry-date");
@@ -123,32 +141,42 @@ function validateExpiryDate() {
     parentElement.appendChild(warning);
   }
 
+  // Clear the existing warning message
   warningText.textContent = null;
 
+  // Split the date input into month and year parts
   const [month, year] = date.value.split("/");
+  // get the current year(at the time of writing this code currentYear = 23)
   const currentYear = new Date().getFullYear() % 100;
 
+  // Validate the format and the month/year values
   if (
     date.value.length !== 5 ||
     isNaN(parseInt(month)) ||
     isNaN(parseInt(year)) ||
     month < 1 ||
     month > 12 ||
-    year < currentYear ||
-    year > currentYear + 10
+    // ensures the year being input is either the current year or greater, not any year before
+    year < currentYear
   ) {
+    // If the input is not valid, display a warning message
     warningText.textContent = "Enter in the given format MM/YY";
     return false;
   }
-
+  // If the input is valid, remove the warning message
   parentElement.removeChild(warning);
   return true;
 }
 
+// Event listeners for form submission buttons
 const submitOne = document.getElementById("submitOne");
 const submitTwo = document.getElementById("submitTwo");
+submitOne.addEventListener("click", validateAndSubmitOne);
+submitTwo.addEventListener("click", validateAndSubmitTwo);
 
+// Function to validate and submit form one (shipping details)
 async function validateAndSubmitOne(e) {
+  // Validate all input fields in form one
   const validFname = await validateName("fname", "fname-container");
   const validStreetName = await validateName("street", "street-container");
   const validCityName = await validateName("city", "city-container");
@@ -158,16 +186,18 @@ async function validateAndSubmitOne(e) {
     10,
     "Invalid Telephone Number"
   );
-
+  // If any field is invalid, prevent form submission and show an alert
   if (
     !(validFname && validStreetName && validCityName && validZip && validTele)
   ) {
     e.preventDefault();
-    alert("Please fill all fields. Hover to guide");
+    alert("Please fill all fields.");
   }
 }
 
+// Function to validate and submit form two (payment details)
 async function validateAndSubmitTwo(e) {
+  // Validate all input fields in form two
   const validCname = await validateName("cname", "cardname-container");
   const validEmail = await validateEmail();
   const validCardNum = await validateNumber(
@@ -177,21 +207,14 @@ async function validateAndSubmitTwo(e) {
   );
   const validExpDate = await validateExpiryDate();
   const validCSV = await validateNumber("csv", 3, "Invalid CSV");
+
   if (!(validCname && validEmail && validCardNum && validExpDate && validCSV)) {
     e.preventDefault();
     alert("Please fill all fields. Hover to guide");
   }
 }
 
-submitOne.addEventListener("click", validateAndSubmitOne);
-submitTwo.addEventListener("click", validateAndSubmitTwo);
-
-//validates form One
-
-//validates form Two
-function validateFormTwo() {}
-
-//disables form One and inserts form two
+// Function to switch to form two (payment details) and disable form one (shipping details)
 function switchFormsOne() {
   document.querySelectorAll(".formOne").forEach((element) => {
     element.style.display = "none";
@@ -205,7 +228,7 @@ function switchFormsOne() {
   }
 }
 
-//redirects user to comments page after successful submission
+// Function to redirect user to the comments page after successful submission
 function switchFormsTwo() {
   window.location.href = "/student_2/feedback_form/comments.html";
 }
